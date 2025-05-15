14a127fd5bd9eaa1b06900e9fc370c9a5f6cc3ea
LAPTOP-R3PKM3PI\TEJAS <tejasthorat703@gmail.com>
2025-05-15 14:27:38 +0530

    this is first

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HomeController {
+
+    @GetMapping
+    public String testing(){
+        return "working";
+    }
+}
