d6709de9d6709a7d4ba472b53f011f94b444eff6
LAPTOP-R3PKM3PI\TEJAS <tejasthorat703@gmail.com>
2025-05-15 19:34:58 +0530

    Repositroy and service

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -4,4 +4,13 @@ import com.ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository  extends JpaRepository<User,String> {
+
+    User findByEmail(String email); //this method not come in jpa repositoy  so we make
+                                    // custome method like these ---using(findBy)
+
+    User findByEmailAndPassword(String email, String password);
+
+    User findByNameContaining(String keywords);
+
+
 }
