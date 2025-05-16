b26f1ec82015cbd2e591840f9fc835ebe6d6d0b4
LAPTOP-R3PKM3PI\TEJAS <tejasthorat703@gmail.com>
2025-05-16 14:40:19 +0530

    Api Response new class for message

---
+++ src/main/java/com/ecommerce/dtos/ApiResponseMessage.java
@@ -0,0 +1,23 @@
+package com.ecommerce.dtos;
+
+//This class is create to send message in json formate
+// example we delete the message and we need to send message
+//so it send in string formate thats why we need to send it in json
+// and for that we created separate class for message
+
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponseMessage {
+
+    private String msg;
+    private boolean success;
+    private HttpStatus status;              //not compulsary depend on us
+
+}
